3ba2cf9da6547bb971df39c3ad8d8ed52744285f
Jenni <109981562+jennial@users.noreply.github.com>
2025-04-25 17:47:23 -0300

    adicionado classes de teste e refatoração

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,44 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  test("deve renderizar o valor inicial como 0", () => {
+    render(<App />);
+    const input = screen.getByDisplayValue("0");
+    expect(input).toBeInTheDocument();
+  });
+
+  test("deve aumentar o valor ao clicar em Adicionar", () => {
+    render(<App />);
+    const btn = screen.getByText("Adicionar");
+    fireEvent.click(btn);
+    const input = screen.getByDisplayValue("1");
+    expect(input).toBeInTheDocument();
+  });
+
+  test("deve diminuir o valor ao clicar em Diminuir", () => {
+    render(<App />);
+    const btnAdd = screen.getByText("Adicionar");
+    const btnSub = screen.getByText("Diminuir");
+
+    fireEvent.click(btnAdd);
+    fireEvent.click(btnSub);
+    const input = screen.getByDisplayValue("0");
+    expect(input).toBeInTheDocument();
+  });
+
+  test("deve renderizar botões numéricos", () => {
+    render(<App />);
+    expect(screen.getByText("7")).toBeInTheDocument();
+    expect(screen.getByText("0")).toBeInTheDocument();
+  });
+
+  test("deve renderizar operadores e reagir a clique", () => {
+    render(<App />);
+    const btn = screen.getByText("+");
+    fireEvent.click(btn);
+    const input = screen.getByDisplayValue("1");
+    expect(input).toBeInTheDocument();
+  });
 });
